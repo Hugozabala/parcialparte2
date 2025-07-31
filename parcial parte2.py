8b17cf49b51be3c1b0614aa1df3071a9aceb9791
Usuario <hugocanizzabala@gmail.com>
2025-07-30 19:34:56 -0600

    menu y funciones 1

--- parcial parte2.py
+++ parcial parte2.py
@@ -7,13 +7,13 @@ def menu():
     print("5. calcular cuantos digitos tiene un numero")
     print("6. salir")
 
-def cadena_rep3(cadena):
+def cadena_rep3(cadena,rep):
     if len(cadena)==0:
         return 0
     else:
         return cadena +cadena +cadena
 
-def cadena_letra_repetida(cadena1):
+def cadena_letra_repetida(cadena1,):
     if len(cadena1)==0:
         return 0
     else:
@@ -21,9 +21,12 @@ def cadena_letra_repetida(cadena1):
 
 def suma_digitos(n):
     if n < 10:
-        return n
+        return 1
     else:
-        return n % 10 + suma_digitos(n // 10)
+        a=n % 10
+        t=0
+        t+=a
+        return a
 
 def mcd_de_numero(num,num2):
     if ((num <=0 )or(num2 <=0)) :
@@ -39,13 +42,17 @@ op=0
 while op!=6:
     menu()
     op=int(input("ingrese opcion a ejecutar"))
+
     match (op):
         case 1:
             num1 =int(input("ingrese primer numero"))
             num2= int(input("ingrse segundo numero"))
+            mcd_de_numero(num1,num2)
+            print(mcd_de_numero(num1,num2))
 
         case 2:
             cad=input("ingrese palabra")
+            veces=int(input("ingrese cuantas veces desea repetir"))
             cadena_rep3(cad)
             print(cadena_rep3(cad))
 
@@ -61,7 +68,7 @@ while op!=6:
             num3=int(input("ingrse un numero entero"))
             suma_digitos(num3)
             print(suma_digitos(num3))
-            break
+
 
         case 6:
             print("fin de programa")

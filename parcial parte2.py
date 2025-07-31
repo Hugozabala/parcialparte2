4f7f3d88b05746c0858c3ce51da2073230fddb10
Usuario <hugocanizzabala@gmail.com>
2025-07-30 18:29:01 -0600

    menu

---
+++ parcial parte2.py
@@ -0,0 +1,33 @@
+def menu():
+    print(" =====menu principal")
+    print("1. Calcular el MCD de dos numeros (recursivos")
+    print("2. crear un cadena repetida n veces")
+    print("3. contar cuantas veces aparece una letra en una cadena ")
+    print("4. convertir un numero decimal a vinario")
+    print("5. calcular cuantos digitos tiene un numero")
+    print("6. salir")
+
+op=0
+while op!=6:
+    op=int(input("ingrese opcion a ejecutar"))
+    match (op):
+        case 1:
+            break
+        case 2:
+            break
+
+        case 3:
+            break
+
+        case 4:
+            break
+
+        case 5:
+            break
+
+        case 6:
+            print("fin de programa")
+
+        case _:
+            print("ingrese una opcion correcta")
+
